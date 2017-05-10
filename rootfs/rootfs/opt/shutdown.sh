4e648ba33440c7c5f981bd9894c708e8832d6b8d
Régis Belson <regis@evaneos.com>
2017-05-10 14:08:53 +0200

    Add un/mount mechanics for docker-machine

--- rootfs/rootfs/opt/shutdown.sh
+++ rootfs/rootfs/opt/shutdown.sh
@@ -4,3 +4,5 @@
 echo "${YELLOW}Running boot2docker shutdown script...${NORMAL}"
 
 /usr/local/etc/init.d/docker stop
+
+zpool export boot2docker-data || zpool export boot2docker-data # might not work the first time for some reason
